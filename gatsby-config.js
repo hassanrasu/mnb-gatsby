f2f44e65600208282aad8f2ef5e9b6b9c8f07f1e
Hassan Rasu <hassanrasu04@gmail.com>
2023-02-28 13:45:17 +0600

    commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,40 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+let activeEnv =
+  process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development";
+
+console.log(`Using environment config: '${activeEnv}'`);
+
+require("dotenv").config({
+  path: `.env.${activeEnv}`,
+});
+
+console.log(`This WordPress Endpoint is used: '${process.env.WORDPRESS_URL}'`);
+
 module.exports = {
   siteMetadata: {
     title: `Mnb`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [{
-    resolve: 'gatsby-source-wordpress',
-    options: {
-      "url": "http://localhost/mnb/graphql"
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp"]
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: "gatsby-source-graphql",
+      options: {
+        typeName: "WPGraphQL",
+        fieldName: "wpgraphql",
+        url: `${process.env.WORDPRESS_URL}/graphql`,
+      },
+    },
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+  ],
 };
